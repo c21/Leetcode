9eec3b70b634f7f6bfc3b0676457c62968e6bbb4
c21 <253546675@qq.com>
2015-10-21 16:22:14 -0500

    Create 107.210 Course Schedule II.cpp

---
+++ medium/107.210 Course Schedule II.cpp
@@ -0,0 +1,81 @@
+/*
+There are a total of n courses you have to take, labeled from 0 to n - 1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
+
+There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
+
+For example:
+
+2, [[1,0]]
+There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1]
+
+4, [[1,0],[2,0],[3,1],[3,2]]
+There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0. So one correct course order is [0,1,2,3]. Another correct ordering is[0,2,1,3].
+
+Note:
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+*/
+
+
+class Solution 
+{
+
+	public:
+
+	vector<int> result;
+
+
+	vector<int> findOrder(int numCourses, vector<pair<int, int> >& prerequisites) 
+	{
+		result = vector<int>();
+		vector<int> visit = vector<int>();
+		for(int i = 0; i < numCourses; i++)
+			visit.push_back(0);
+		
+		vector<vector<int> > edge = vector<vector<int> >();
+		for(int i = 0; i < numCourses; i++)
+		{
+			edge.push_back(vector<int>());
+			for(int j = 0; j < prerequisites.size(); j++)
+			{
+				if(prerequisites[j].first == i)
+					edge[i].push_back(prerequisites[j].second);
+			}	
+		}
+		for(int i = 0; i < numCourses; i++)
+		{
+			if(visit[i] == 0)
+			{
+				if(!search(edge, visit, i))
+					return vector<int>();
+			}	
+		}	
+		return result;				
+	}
+
+
+	bool search(vector<vector<int> >& edge, vector<int>& visit, int k)
+	{
+		visit[k] = 1;
+		for(int i = 0; i < edge[k].size(); i++)
+		{
+			if(visit[edge[k][i]] == 1)
+				return false;
+			else if(visit[edge[k][i]] == 0)
+			{
+				if(!search(edge, visit, edge[k][i]))
+					return false;
+			}
+		}
+		result.push_back(k);
+		visit[k] = 2;
+		return true;	
+	}
+};
+
+/*
+  拓扑排序，可以用深度优先搜索。
+*/
