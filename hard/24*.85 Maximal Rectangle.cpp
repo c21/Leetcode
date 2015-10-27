a8880303c6bc8a66c4bf1a1845306d527dba802d
c21 <253546675@qq.com>
2015-10-27 11:49:53 -0500

    Create 24*.85 Maximal Rectangle.cpp

---
+++ hard/24*.85 Maximal Rectangle.cpp
@@ -0,0 +1,77 @@
+/*
+Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing all ones and return its area.
+*/
+
+class Solution 
+{
+
+	public:
+
+	int maximalRectangle(vector<vector<char> >& matrix) 
+	{
+		if(matrix.size() == 0 || matrix[0].size() == 0)
+			return false;
+		int row = matrix.size();
+		int col = matrix[0].size();
+
+		vector<int> height = vector<int>();
+		int max_area = 0;
+		for(int i = 0; i < row; i++)
+		{
+			if(i == 0)
+			{
+				for(int j = 0; j < col; j++)
+				{
+					if(matrix[i][j] == '0')
+						height.push_back(0);
+					else
+						height.push_back(1);
+				}
+			}	
+			else
+			{
+				for(int j = 0; j < col; j++)
+        {
+          if(matrix[i][j] == '0')
+            height[j] = 0;
+          else
+            height[j]++;
+        }
+			}
+
+			int this_a = get_area(height);
+			max_area = max(max_area, this_a);	
+		}
+
+		return max_area;					
+	}
+
+	int get_area(vector<int>& height)
+	{
+		int max_area = 0;
+		for(int i = 0; i < height.size(); i++)
+		{
+			if(i+1 < height.size() && height[i] <= height[i+1])
+				continue;
+			int min_num = height[i];
+			int j = i;
+			while(j >= 0)
+			{
+				if(height[j] < min_num)
+					min_num = height[j];
+				int this_a = min_num * (i-j+1);
+				max_area = max(max_area, this_a);
+				j--;
+			}		
+		}		
+		return max_area;
+	}	
+};
+
+/*
+  这道题没想出来，用动态规划求正方形的方法好像没法做。
+  看了答案发现，依次对每一行作为矩形的最下边，统计每列以最下边为底边的连续1的个数。
+  仿照求直方图最大面积的方法，求出的面积即为以该行为最下边的矩形的最大面积。
+  依次枚举所有行，得到最大矩形。
+  这里求直方图用的是O(n^2)的方法，总时间复杂度为O(n^3). 求直方图有O(n)方法，以后再做。
+*/
