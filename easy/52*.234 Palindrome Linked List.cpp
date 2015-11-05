6e3108e50745bd77afcd4aaa5e5ebf50bfd971c0
c21 <253546675@qq.com>
2015-11-04 20:40:16 -0600

    Update 52*.234 Palindrome Linked List.cpp

--- easy/52*.234 Palindrome Linked List.cpp
+++ easy/52*.234 Palindrome Linked List.cpp
@@ -35,5 +35,66 @@ public:
 };
 
 /*
-  没有使用O(1)空间，而是使用O(n)空间。使用O(1)空间需要将链表后半部分翻转。
+    更新的方法如下：
+    1.找到中点(用快慢指针方法)
+    2.翻转前半部链表
+    3.比较前后两半链表
 */
+
+class Solution 
+{
+public:
+    bool isPalindrome(ListNode* head) 
+    {
+        // corner case
+        if(head == NULL || head->next == NULL)
+        	return true;
+        
+        // find the midpoint of linked list
+        int len = 0;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(slow != NULL && fast != NULL)
+        {
+        	slow = slow->next;
+        	fast = fast->next;
+        	len++;
+        	if(fast != NULL)
+        	{
+        		fast = fast->next;
+        		len++;
+        	}
+        }
+        
+        // reverse the linked list from head to slow
+        ListNode* p = reverse(head, slow);
+        if(len % 2 == 1)
+        	p = p->next;
+        while(p != NULL && slow != NULL)
+        {
+        	if(p->val != slow->val)
+        		return false;
+        	p = p->next;
+        	slow = slow->next;
+        }
+        return true;
+     }
+
+	ListNode* reverse(ListNode* head, ListNode* end)
+	{
+		if(head == NULL || head->next == end)
+			return head;
+		ListNode* h = head;
+		ListNode* prev = head;
+		ListNode* p = head->next;
+        while(p != end)
+		{
+			ListNode* next = p->next;
+			p->next = h;
+			prev->next = next;
+			h = p;
+			p = next;
+		}
+		return h;
+	}
+};
