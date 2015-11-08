5814a7be5ba354019f6cc725c43b0d802f37c281
c21 <253546675@qq.com>
2015-11-08 10:39:48 -0600

    Update 3*.260 Single Number III.cpp

--- medium/3*.260 Single Number III.cpp
+++ medium/3*.260 Single Number III.cpp
@@ -38,3 +38,52 @@ class Solution
 /*
   这里使用unordered_map方法，如果要O(1)空间复杂度，需要用异或。
 */
+
+/*
+	因为找到的两个数不同，所以这两个数异或的结果不为0，按照某个为1的位，分别进行全部数的异或。
+	使得这两个数分开，得到结果。空间复杂度为O(1).
+*/
+
+
+class Solution 
+{
+    public:
+
+	int find_one(int x)
+	{
+		int n = 0;
+		while((x & 1) != 1)
+		{
+			x = (x >> 1);
+			n++;
+		}
+		return n;
+	}
+
+	
+    vector<int> singleNumber(vector<int>& nums) 
+    {
+    	int x = 0;
+    	for(int i = 0; i < nums.size(); i++)
+    		x = (x ^ nums[i]);
+    	
+    	int n = find_one(x);
+    	int x1 = 0;
+    	int x2 = 0;
+    	for(int i = 0; i < nums.size(); i++)
+    	{
+    		if(((nums[i] >> n) & 1) == 1)
+    			x1 = (x1 ^ nums[i]);
+            else
+            		x2 = (x2 ^ nums[i]); 
+    	}
+    	vector<int> r;
+    	r.push_back(x1);
+    	r.push_back(x2);
+    	return r;
+    }
+};
+
+
+
+
