ba70646b0f3e191c2fab10bb469e941cff34196a
c21 <253546675@qq.com>
2015-11-07 19:47:55 -0600

    Update 7*.141 Linked List Cycle.cpp

--- medium/7*.141 Linked List Cycle.cpp
+++ medium/7*.141 Linked List Cycle.cpp
@@ -57,5 +57,27 @@ class Solution
 /*
   想到的方法是：破坏链表结构，将遍历过的结点都指向自己，如果遇到一个结点指向自己，
   它如果不是遍历过的结点，就是自己本身形成环，这两种情况下该链表都有环。
-  看到可以用快慢指针做，以后再看。
+  看到可以用快慢指针做，如下。如果快慢指针指向同一个地方，就有环。
 */
+
+class Solution 
+{
+    public:
+    bool hasCycle(ListNode *head) 
+    {
+    	if(head == NULL)
+    		return false;
+    	ListNode* slow = head;
+    	ListNode* fast = head;
+    	while(slow != NULL && fast != NULL)
+    	{
+    		slow = slow->next;
+    		fast = fast->next;
+    		if(fast != NULL)
+    			fast = fast->next;
+    		if(slow == fast && slow != NULL)
+    			return true;
+    	}
+    	return false;
+    }
+};
