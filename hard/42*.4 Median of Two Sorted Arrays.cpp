7691883c700a0183834083998b3d9608aae780d8
c21 <253546675@qq.com>
2015-11-09 17:30:13 -0600

    Update 42*.4 Median of Two Sorted Arrays.cpp

--- hard/42*.4 Median of Two Sorted Arrays.cpp
+++ hard/42*.4 Median of Two Sorted Arrays.cpp
@@ -90,3 +90,43 @@ class Solution
   如果a[k/2]<b[k/2], 那么a[1-k/2]都不会为第k小的数。
   如果a[k/2]>b[k/2], 那么b[1-k/2]都不会为第k小的数。
 */
+
+
+class Solution 
+{
+        public:
+    	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
+    	{
+            int m = nums1.size(), n = nums2.size();
+            if((m+n) % 2 != 0)
+                return find(nums1, nums2, 0, m-1, 0, n-1, (m+n)/2+1);
+            else
+            {
+            	double r1 = find(nums1, nums2, 0, m-1, 0, n-1, (m+n)/2);
+            	double r2 = find(nums1, nums2, 0, m-1, 0, n-1, (m+n)/2+1);
+            	return (r1+r2)/2;
+            }	
+    	}
+
+    	double find(vector<int>& n1, vector<int>& n2, int b1, int e1, int b2, int e2, int k)
+    	{
+    		if(e1-b1 > e2-b2)
+    			return find(n2, n1, b2, e2, b1, e1, k);
+    		else if(b1 > e1)
+    			return n2[b2+k-1];
+    		else if(k == 1)
+    			return min(n1[b1], n2[b2]);
+    		else
+    		{
+    			int o1 = min(k/2, e1-b1+1);
+    			int o2 = k-o1;
+    			if(n1[b1+o1-1] == n2[b2+o2-1])
+    				return n1[b1+o1-1];
+    			else if(n1[b1+o1-1] < n2[b2+o2-1])
+    				return find(n1,n2,b1+o1,e1,b2,e2,k-o1);
+    			else
+    				return find(n1,n2,b1,e1,b2+o2,e2,k-o2);
+    		}
+    	}
+};
+
